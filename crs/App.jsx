58c639281f281c36f417b34b4b5c2bd810bd5384
ryu <naragurumenetrpg1@gmail.com>
2025-12-11 22:36:25 +0900

    Delete crs directory

--- crs/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function App() {
-  return (
-    <div className="w-full h-screen flex items-center justify-center text-3xl">
-      Battle-A Ready 🚀
-    </div>
-  );
-}
