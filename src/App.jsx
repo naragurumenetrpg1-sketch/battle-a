68c1d48ff92fbb4965c976309230c3d667ab3d91
ryu <naragurumenetrpg1@gmail.com>
2025-12-11 22:37:14 +0900

    Create App component with initial layout

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center text-3xl">
+      Battle-A Ready 🚀
+    </div>
+  );
+}
